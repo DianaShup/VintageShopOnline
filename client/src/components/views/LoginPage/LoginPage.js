4317b3a75fab674b2b1aa45cfee0962dc472139b
Vladislove <vladislav.lapko@gmail.com>
2021-06-01 16:18:33 +0200

    OAuth2  added

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -5,129 +5,154 @@ import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { Form, Input, Button, Checkbox, Typography } from 'antd';
 import { useDispatch } from "react-redux";
+import { GoogleLogin } from 'react-google-login';
 
 const { Title } = Typography;
 
 function LoginPage(props) {
-  const dispatch = useDispatch();
-  const [formErrorMessage, setFormErrorMessage] = useState('')
-  const initialEmail = localStorage.getItem("rememberMe") ? localStorage.getItem("rememberMe") : '';
-  
-  return (
-    <Formik
-      initialValues={{
-        email: initialEmail,
-        password: '',
-      }}
+    const dispatch = useDispatch();
+    const [formErrorMessage, setFormErrorMessage] = useState('')
+    const initialEmail = localStorage.getItem("rememberMe") ? localStorage.getItem("rememberMe") : '';
+    const handleLogin = async googleData => {
+        const res = await fetch("/api/v1/auth/google", {
+            method: "POST",
+            body: JSON.stringify({
+                token: googleData.tokenId
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await res.json();
+        // store returned user somehow
+    };
 
-      validationSchema={Yup.object().shape({
-        email: Yup.string()
-          .email('Niepoprawny Email')
-          .required('Trzeba podać email'),
-        password: Yup.string()
-          .min(6, 'Minimalna długość hasła 6 znaków')
-          .required('Trzeba podać hasło'),
-      })}
 
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          let dataToSubmit = {
-            email: values.email,
-            password: values.password,
-          };
+    return (
+        <Formik
+            initialValues={{
+                email: initialEmail,
+                password: '',
+            }}
 
-          dispatch(loginUser(dataToSubmit))
-            .then(response => {
-              if (response.payload.loginSuccess) {
-                window.localStorage.setItem('userId', response.payload.userId);
-                if(response.payload.isAdmin || response.payload.role == 1){
-                  props.history.push("/admin");
-                }
-                else{
-                  props.history.push("/");
-                }
-              } else {
-                setFormErrorMessage('Niepoprawny email lub hasło')
-              }
-            })
-            .catch(err => {
-              setFormErrorMessage('Niepoprawny email lub hasło')
-              setTimeout(() => {
-                setFormErrorMessage("")
-              }, 3000);
-            });
-          setSubmitting(false);
-        }, 500);
-      }}
-    >
-      {props => {
-        const {
-          values,
-          touched,
-          errors,
-          isSubmitting,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-        } = props;
+            validationSchema={Yup.object().shape({
+                email: Yup.string()
+                    .email('Niepoprawny Email')
+                    .required('Trzeba podać email'),
+                password: Yup.string()
+                    .min(6, 'Minimalna długość hasła 6 znaków')
+                    .required('Trzeba podać hasło'),
+            })}
 
-        return (
-          <div className="app">
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    let dataToSubmit = {
+                        email: values.email,
+                        password: values.password,
+                    };
 
-            <Title level={2}>Logowanie</Title>
-            <form onSubmit={handleSubmit} style={{ width: '600px' }}>
+                    dispatch(loginUser(dataToSubmit))
+                        .then(response => {
+                            if (response.payload.loginSuccess) {
+                                window.localStorage.setItem('userId', response.payload.userId);
+                                if (response.payload.isAdmin || response.payload.role == 1) {
+                                    props.history.push("/admin");
+                                }
+                                else {
+                                    props.history.push("/");
+                                }
+                            } else {
+                                setFormErrorMessage('Niepoprawny email lub hasło')
+                            }
+                        })
+                        .catch(err => {
+                            setFormErrorMessage('Niepoprawny email lub hasło')
+                            setTimeout(() => {
+                                setFormErrorMessage("")
+                            }, 3000);
+                        });
+                    setSubmitting(false);
+                }, 500);
+            }}
+        >
+            {props => {
+                const {
+                    values,
+                    touched,
+                    errors,
+                    isSubmitting,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                } = props;
 
-              <Form.Item required>
-                <Input
-                  id="email"
-                  placeholder="Email"
-                  type="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={
-                    errors.email && touched.email ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.email && touched.email && (
-                  <div className="input-feedback">{errors.email}</div>
-                )}
-              </Form.Item>
+                return (
+                    <div className="app">
 
-              <Form.Item required>
-                <Input
-                  id="password"
-                  placeholder="Hasło"
-                  type="password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={
-                    errors.password && touched.password ? 'text-input error' : 'text-input'
-                  }
-                />
-                {errors.password && touched.password && (
-                  <div className="input-feedback">{errors.password}</div>
-                )}
-              </Form.Item>
+                        <Title level={2}>Logowanie</Title>
+                        <form onSubmit={handleSubmit} style={{ width: '600px' }}>
 
-              {formErrorMessage && (
-                <label ><p style={{ color: '#ff0000bf', border: '1px solid', padding: '1rem' }}>{formErrorMessage}</p></label>
-              )}
+                            <Form.Item required>
+                                <Input
+                                    id="email"
+                                    placeholder="Email"
+                                    type="email"
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    className={
+                                        errors.email && touched.email ? 'text-input error' : 'text-input'
+                                    }
+                                />
+                                {errors.email && touched.email && (
+                                    <div className="input-feedback">{errors.email}</div>
+                                )}
+                            </Form.Item>
 
-              <Form.Item>
-                <div>
-                  <Button type="primary" htmlType="submit" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
-                    Log in
-                </Button>
-                </div>
-              </Form.Item>
-            </form>
-          </div>
-        );
-      }}
-    </Formik>
-  );
+                            <Form.Item required>
+                                <Input
+                                    id="password"
+                                    placeholder="Hasło"
+                                    type="password"
+                                    value={values.password}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    className={
+                                        errors.password && touched.password ? 'text-input error' : 'text-input'
+                                    }
+                                />
+                                {errors.password && touched.password && (
+                                    <div className="input-feedback">{errors.password}</div>
+                                )}
+                            </Form.Item>
+
+                            {formErrorMessage && (
+                                <label ><p style={{ color: '#ff0000bf', border: '1px solid', padding: '1rem' }}>{formErrorMessage}</p></label>
+                            )}
+
+                            <Form.Item>
+                                <div>
+                                    <Button type="primary" htmlType="submit" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
+                                        Log in
+                                    </Button>
+                                </div>
+                            </Form.Item>
+                        </form>
+
+                        <GoogleLogin
+                            clientId={process.env.REACT_APP_CLIENT_ID_C}
+                            buttonText="Log in with Google"
+                            onSuccess={handleLogin}
+                            onFailure={handleLogin}
+                            cookiePolicy={'single_host_origin'}
+                        />
+                    </div>
+                );
+            }}
+        </Formik>
+
+
+    );
 };
 
 export default withRouter(LoginPage);
